de9ecfe57bace1a489f552618639655309f1f2d0
vtu24643-eng <vtu24643@veltech.edu.in>
2026-03-24 17:45:17 +0530

    Implement notification services and controller

--- java week 4/week 4.4.java
+++ java week 4/week 4.4.java
@@ -1,1 +1,46 @@
+//NotificationService.java
+package com.yourpackage.service;
+public interface NotificationService {
+    String sendNotification();
+}
 
+//EmailNotificationService.java
+package com.yourpackage.service;
+import org.springframework.stereotype.Service;
+@Service("emailService")   
+public class EmailNotificationService implements NotificationService {
+    @Override
+    public String sendNotification() {
+        return "Email Notification Sent";
+    }
+}
+
+//SMSNotificationService.java
+package com.yourpackage.service;
+import org.springframework.stereotype.Service;
+@Service("smsService")   
+public class SMSNotificationService implements NotificationService {
+    @Override
+    public String sendNotification() {
+        return "SMS Notification Sent";
+    }
+}
+
+//NotificationController.java (Using @Qualifier)
+package com.yourpackage.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.yourpackage.service.NotificationService;
+
+@RestController
+public class NotificationController {
+    @Autowired
+    @Qualifier("emailService")   
+    private NotificationService notificationService;
+    @GetMapping("/notify")
+    public String sendNotification() {
+        return notificationService.sendNotification();
+    }
+}
