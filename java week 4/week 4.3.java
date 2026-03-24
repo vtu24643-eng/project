08f9d42acd6a6dd5ae7e5dfe1951e2c00cd74f09
vtu24643-eng <vtu24643@veltech.edu.in>
2026-03-24 17:37:58 +0530

    Implement TaskService with controller and service

--- java week 4/week 4.3.java
+++ java week 4/week 4.3.java
@@ -1,1 +1,36 @@
+//TaskService.java (Interface)
+package com.yourpackage.service;
+public interface TaskService {
+    String getTask();
+}
 
+//TaskServiceImpl.java
+package com.yourpackage.service;
+import org.springframework.stereotype.Service;
+@Service
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public String getTask() {
+        return "Task fetched successfully";
+    }
+}
+
+//TaskController.java (Constructor Injection)
+package com.yourpackage.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.yourpackage.service.TaskService;
+
+@RestController
+public class TaskController {
+    private TaskService taskService;
+    @Autowired  
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    @GetMapping("/task")
+    public String getTask() {
+        return taskService.getTask();
+    }
+}
