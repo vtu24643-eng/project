f6b44d48565b314364a0002fa2734afad165fbc0
vtu24643-eng <vtu24643@veltech.edu.in>
2026-03-25 08:46:34 +0530

    Add AppInitializer for Spring MVC configuration

--- java week 4/week 4.7.java
+++ java week 4/week 4.7.java
@@ -49,3 +49,21 @@ public class WebConfig {
         return resolver;
     }
 }
+
+//Dispatcher Initialization
+package com.yourpackage.config;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
