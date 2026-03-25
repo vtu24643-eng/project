f07b9fa20983c4eb30400c48c6c4040126668600
vtu24643-eng <vtu24643@veltech.edu.in>
2026-03-25 08:55:46 +0530

    Add Spring Boot application with database connection

--- java week 5/week 5.1.java
+++ java week 5/week 5.1.java
@@ -1,1 +1,52 @@
+//Application.java
+package com.yourpackage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+//DBController.java
+package com.yourpackage.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DBController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @GetMapping("/test-db")
+    public String testDB() {
+        jdbcTemplate.execute("SELECT 1");
+        return "Database Connected Successfully!";
+    }
+}
+
+//application.properties
+server.port=8081
+spring.datasource.url=jdbc:mysql://localhost:3306/your_db
+spring.datasource.username=root
+spring.datasource.password=your_password
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+//pom.xml
+  <dependencies>
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-web</artifactId>
+    </dependency>
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-jdbc</artifactId>
+    </dependency>
+    <dependency>
+        <groupId>com.mysql</groupId>
+        <artifactId>mysql-connector-j</artifactId>
+    </dependency>
+</dependencies>
