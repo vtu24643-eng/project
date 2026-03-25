8f04ece13a3e5024c6d406095f599191f8713233
vtu24643-eng <vtu24643@veltech.edu.in>
2026-03-25 10:08:01 +0530

    Update week 6.2.java

--- java week 6/week 6.2.java
+++ java week 6/week 6.2.java
@@ -1,1 +1,32 @@
+//TaskController
+package com.yourpackage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.yourpackage.model.Task;
+import com.yourpackage.service.TaskService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService service;
+
+    @PostMapping
+    public Task addTask(@RequestBody Task t) {
+        return service.addTask(t);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return service.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable int id) {
+        return service.getTaskById(id);
+  }
+  
+}
